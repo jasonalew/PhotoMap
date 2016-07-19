44dc43119889bacecf2ddd300330d14814d9b8e2
Jason Lew <jason.a.lew@gmail.com>
2016-07-19 13:04:47 -0700

    Nil out image and path for reuse

--- PhotoMap/Views/PhotoAnnotationView.swift
+++ PhotoMap/Views/PhotoAnnotationView.swift
@@ -13,20 +13,29 @@ class PhotoAnnotationView: MKAnnotationView {
     struct Margin {
         static let standard: CGFloat = 2.0
     }
+    
+    lazy var imageView = UIImageView()
+    var fullSizeImagePath: String?
 
     override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
         super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
         self.backgroundColor = UIColor.whiteColor()
-        self.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        self.frame = CGRect(x: 0, y: 0, width: 60, height: 60)
         if let annotation = annotation as? Photo,
-        let path = annotation.imagePath,
+        let path = annotation.thumbImagePath,
             let url = NSURL(string: path),
         let data = NSData(contentsOfURL: url),
         let image = UIImage(data: data) {
-            let imageView = UIImageView(image: image)
+            self.fullSizeImagePath = annotation.fullSizeImagePath
+            imageView.image = image
             imageView.frame = CGRect(x: Margin.standard, y: Margin.standard, width: self.frame.width - 2 * Margin.standard, height: self.frame.height - 2 * Margin.standard)
             self.addSubview(imageView)
         }
+    }
+    
+    override func prepareForReuse() {
+        imageView.image = nil
+        fullSizeImagePath = nil
     }
     
     required init?(coder aDecoder: NSCoder) {
