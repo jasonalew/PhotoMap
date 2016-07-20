60d07348e0378ec83986d9ca6fa86c54c853a166
Jason Lew <jason.a.lew@gmail.com>
2016-07-20 09:49:36 -0700

    Removed image loading on init as this will be handled in the mapView delegate

--- PhotoMap/Views/PhotoAnnotationView.swift
+++ PhotoMap/Views/PhotoAnnotationView.swift
@@ -10,8 +10,9 @@ import UIKit
 import MapKit
 
 class PhotoAnnotationView: MKAnnotationView {
-    struct Margin {
-        static let standard: CGFloat = 2.0
+    struct Layout {
+        static let marginStandard: CGFloat = 2.0
+        static let annotationEdge: CGFloat = 60
     }
     
     lazy var imageView = UIImageView()
@@ -20,17 +21,12 @@ class PhotoAnnotationView: MKAnnotationView {
     override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
         super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
         self.backgroundColor = UIColor.whiteColor()
-        self.frame = CGRect(x: 0, y: 0, width: 60, height: 60)
-        if let annotation = annotation as? Photo,
-        let path = annotation.thumbImagePath,
-            let url = NSURL(string: path),
-        let data = NSData(contentsOfURL: url),
-        let image = UIImage(data: data) {
-            self.fullSizeImagePath = annotation.fullSizeImagePath
-            imageView.image = image
-            imageView.frame = CGRect(x: Margin.standard, y: Margin.standard, width: self.frame.width - 2 * Margin.standard, height: self.frame.height - 2 * Margin.standard)
-            self.addSubview(imageView)
-        }
+        self.frame = CGRect(x: 0, y: 0, width: Layout.annotationEdge, height: Layout.annotationEdge)
+        imageView.frame = CGRect(x: Layout.marginStandard,
+                                 y: Layout.marginStandard,
+                                 width: self.frame.width - 2 * Layout.marginStandard,
+                                 height: self.frame.height - 2 * Layout.marginStandard)
+        self.addSubview(imageView)
     }
     
     override func prepareForReuse() {
